b99cb86e4068cc838418770acd13cb0b5b14f1c9
Spookymovingnnue <79128122+Spookymovingnnue@users.noreply.github.com>
2021-02-24 10:50:13 +0530

    Update startbot.sh

--- startbot.sh
+++ startbot.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-chmod a+x engines/stockfish13.exe
+chmod #!/bin/bash
+chmod a+x engines/stockfish-9-64
+ls engines -l
+chmod +x ./engines/stockfish13.exe
 ls engines -l
 python lichess-bot.py
